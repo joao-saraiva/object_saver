85eaa18f977c627bb49a5a9af428905e530cfe63
joão <joaovsaraivasousa@gmail.com>
2022-08-30 18:08:05 -0300

    initial commit

---
+++ lib/object_saver.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "object_saver/version"
+
+module ObjectSaver
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "object_saver"
+
+require "minitest/autorun"
